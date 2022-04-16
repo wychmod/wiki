68214f018cca57bbff26ad0bfd80b33accbd2696
wychmod <545480453@qq.com>
2022-04-16 15:59:47 +0800

    2-7 增加http测试

--- src/main/java/com/wychmod/wiki/controller/TestController.java
+++ src/main/java/com/wychmod/wiki/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.wychmod.wiki.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +10,10 @@ public class TestController {
     @RequestMapping("/hello")
     public String hello() {
         return "hello world";
+    }
+
+    @PostMapping("/hello/post")
+    public String helloPost(String name) {
+        return "hello world! post"+name;
     }
 }
