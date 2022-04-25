b869d84c9e8a1192ee38eae41bce6b9914967a9a
wychmod <545480453@qq.com>
2022-04-25 17:22:02 +0800

    6-4 集成PageHelper插件，将mapper日志级别改成trace，打印所有的sql日志

--- src/main/java/com/wychmod/wiki/service/EbookService.java
+++ src/main/java/com/wychmod/wiki/service/EbookService.java
@@ -1,11 +1,15 @@
 package com.wychmod.wiki.service;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.wychmod.wiki.domain.Ebook;
 import com.wychmod.wiki.domain.EbookExample;
 import com.wychmod.wiki.mapper.EbookMapper;
 import com.wychmod.wiki.req.EbookReq;
 import com.wychmod.wiki.resp.EbookResp;
 import com.wychmod.wiki.util.CopyUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
 
@@ -17,15 +21,23 @@ public class EbookService {
     @Resource
     private EbookMapper ebookMapper;
 
+    private static final Logger LOG = LoggerFactory.getLogger(EbookService.class);
+
     public List<EbookResp> list(EbookReq req) {
         EbookExample ebookExample = new EbookExample();
         EbookExample.Criteria criteria = ebookExample.createCriteria();
         if (!ObjectUtils.isEmpty(req.getName())){
             criteria.andNameLike("%"+req.getName()+"%");
         }
-        List<Ebook> ebooklist = ebookMapper.selectByExample(ebookExample);
+        // 分页 pagenum查第几页 pagesize查几条 一条pagehelper只对遇见的第一个查询有效
+        PageHelper.startPage(1,3);
+        List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
+        // PageInfo 可以获得总行数和总页数
+        PageInfo<Ebook> pageInfo = new PageInfo<>(ebookList);
+        LOG.info("总行数：{}",pageInfo.getTotal());
+        LOG.info("总页数：{}",pageInfo.getPages());
 
-        List<EbookResp> list = CopyUtil.copyList(ebooklist, EbookResp.class);
+        List<EbookResp> list = CopyUtil.copyList(ebookList, EbookResp.class);
         return list;
     }
 }
