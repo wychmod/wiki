15ab9ef1203333bf206d140914c538b7cb9e75bc
wychmod <545480453@qq.com>
2022-04-25 21:22:48 +0800

    6-5 封装分页请求参数PageReq和返回参数PageResp

--- src/main/java/com/wychmod/wiki/controller/EbookController.java
+++ src/main/java/com/wychmod/wiki/controller/EbookController.java
@@ -3,13 +3,13 @@ package com.wychmod.wiki.controller;
 import com.wychmod.wiki.req.EbookReq;
 import com.wychmod.wiki.resp.CommonResp;
 import com.wychmod.wiki.resp.EbookResp;
+import com.wychmod.wiki.resp.PageResp;
 import com.wychmod.wiki.service.EbookService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
-import java.util.List;
 
 @RestController
 @RequestMapping("/ebook") //访问公共前缀
@@ -19,8 +19,8 @@ public class EbookController {
 
     @GetMapping ("/list")
     public CommonResp list(EbookReq req) {
-        CommonResp<List<EbookResp>> resp = new CommonResp<>();
-        List<EbookResp> list = ebookService.list(req);
+        CommonResp<PageResp<EbookResp>> resp = new CommonResp<>();
+        PageResp<EbookResp> list = ebookService.list(req);
         resp.setContent(list);
         return resp;
     }
--- src/main/java/com/wychmod/wiki/service/EbookService.java
+++ src/main/java/com/wychmod/wiki/service/EbookService.java
@@ -7,6 +7,7 @@ import com.wychmod.wiki.domain.EbookExample;
 import com.wychmod.wiki.mapper.EbookMapper;
 import com.wychmod.wiki.req.EbookReq;
 import com.wychmod.wiki.resp.EbookResp;
+import com.wychmod.wiki.resp.PageResp;
 import com.wychmod.wiki.util.CopyUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -23,14 +24,14 @@ public class EbookService {
 
     private static final Logger LOG = LoggerFactory.getLogger(EbookService.class);
 
-    public List<EbookResp> list(EbookReq req) {
+    public PageResp<EbookResp> list(EbookReq req) {
         EbookExample ebookExample = new EbookExample();
         EbookExample.Criteria criteria = ebookExample.createCriteria();
         if (!ObjectUtils.isEmpty(req.getName())){
             criteria.andNameLike("%"+req.getName()+"%");
         }
         // 分页 pagenum查第几页 pagesize查几条 一条pagehelper只对遇见的第一个查询有效
-        PageHelper.startPage(1,3);
+        PageHelper.startPage(req.getPage(), req.getSize());
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
         // PageInfo 可以获得总行数和总页数
         PageInfo<Ebook> pageInfo = new PageInfo<>(ebookList);
@@ -38,6 +39,10 @@ public class EbookService {
         LOG.info("总页数：{}",pageInfo.getPages());
 
         List<EbookResp> list = CopyUtil.copyList(ebookList, EbookResp.class);
-        return list;
+        PageResp<EbookResp> pageResp = new PageResp();
+        pageResp.setTotal(pageInfo.getTotal());
+        pageResp.setList(list);
+
+        return pageResp;
     }
 }
