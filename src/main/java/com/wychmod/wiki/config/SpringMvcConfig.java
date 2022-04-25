f55f158be2ea7b8ff1f02d69b438ffe2b040a108
wychmod <545480453@qq.com>
2022-04-25 11:38:38 +0800

    5-9 增加aop 添加接口耗时

--- src/main/java/com/wychmod/wiki/config/SpringMvcConfig.java
+++ src/main/java/com/wychmod/wiki/config/SpringMvcConfig.java
@@ -1,22 +1,22 @@
-package com.wychmod.wiki.config;
-
-import com.wychmod.wiki.interceptor.LogInterceptor;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import javax.annotation.Resource;
-
-@Configuration
-public class SpringMvcConfig implements WebMvcConfigurer {
-
-    @Resource
-    LogInterceptor logInterceptor;
-
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(logInterceptor)
-                .addPathPatterns("/**");
-
-    }
-}
+//package com.wychmod.wiki.config;
+//
+//import com.wychmod.wiki.interceptor.LogInterceptor;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//import javax.annotation.Resource;
+//
+//@Configuration
+//public class SpringMvcConfig implements WebMvcConfigurer {
+//
+//    @Resource
+//    LogInterceptor logInterceptor;
+//
+//    @Override
+//    public void addInterceptors(InterceptorRegistry registry) {
+//        registry.addInterceptor(logInterceptor)
+//                .addPathPatterns("/**");
+//
+//    }
+//}
--- src/main/java/com/wychmod/wiki/filter/LogFilter.java
+++ src/main/java/com/wychmod/wiki/filter/LogFilter.java
@@ -1,34 +1,34 @@
-package com.wychmod.wiki.filter;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Component;
-
-// 过滤器是servlet里面的一个概念,servlet又是容器的概念,过滤器给容器用的
-import javax.servlet.*;
-import javax.servlet.http.HttpServletRequest;
-import java.io.IOException;
-
-@Component
-public class LogFilter implements Filter {
-
-    private static final Logger LOG = LoggerFactory.getLogger(LogFilter.class);
-
-    @Override
-    public void init(FilterConfig filterConfig) throws ServletException {
-
-    }
-
-    @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        // 打印请求信息
-        HttpServletRequest request = (HttpServletRequest) servletRequest;
-        LOG.info("------------- LogFilter 开始 -------------");
-        LOG.info("请求地址: {} {}", request.getRequestURL().toString(), request.getMethod());
-        LOG.info("远程地址: {}", request.getRemoteAddr());
-
-        long startTime = System.currentTimeMillis();
-        // 过滤器链条，如果有其他的会继续执行其他的
-        filterChain.doFilter(servletRequest, servletResponse);
-        LOG.info("------------- LogFilter 结束 耗时：{} ms -------------", System.currentTimeMillis() - startTime);
-    }
-}
+//package com.wychmod.wiki.filter;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.stereotype.Component;
+//
+//// 过滤器是servlet里面的一个概念,servlet又是容器的概念,过滤器给容器用的
+//import javax.servlet.*;
+//import javax.servlet.http.HttpServletRequest;
+//import java.io.IOException;
+//
+//@Component
+//public class LogFilter implements Filter {
+//
+//    private static final Logger LOG = LoggerFactory.getLogger(LogFilter.class);
+//
+//    @Override
+//    public void init(FilterConfig filterConfig) throws ServletException {
+//
+//    }
+//
+//    @Override
+//    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+//        // 打印请求信息
+//        HttpServletRequest request = (HttpServletRequest) servletRequest;
+//        LOG.info("------------- LogFilter 开始 -------------");
+//        LOG.info("请求地址: {} {}", request.getRequestURL().toString(), request.getMethod());
+//        LOG.info("远程地址: {}", request.getRemoteAddr());
+//
+//        long startTime = System.currentTimeMillis();
+//        // 过滤器链条，如果有其他的会继续执行其他的
+//        filterChain.doFilter(servletRequest, servletResponse);
+//        LOG.info("------------- LogFilter 结束 耗时：{} ms -------------", System.currentTimeMillis() - startTime);
+//    }
+//}
