a1577c882c2fc768bdda2d739e49692c50fd8c50
wychmod <545480453@qq.com>
2022-04-25 17:00:29 +0800

    6-3 电子书管理表格展示数据

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -16,5 +16,6 @@ module.exports = {
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'vue/no-unused-components': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'vue/no-unused-vars': 'off',
   }
 }
