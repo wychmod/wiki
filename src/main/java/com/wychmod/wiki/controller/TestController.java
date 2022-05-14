4c00bfcfd30e196cf97073eb37403658826d01d9
wychmod <545480453@qq.com>
2022-05-14 19:18:36 +0800

    9-9 增加 redis 测试接口

--- src/main/java/com/wychmod/wiki/controller/TestController.java
+++ src/main/java/com/wychmod/wiki/controller/TestController.java
@@ -2,17 +2,40 @@ package com.wychmod.wiki.controller;
 
 import com.wychmod.wiki.domain.Test;
 import com.wychmod.wiki.service.TestService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 public class TestController {
     @Resource
     private TestService testService;
+
+    private static final Logger LOG = LoggerFactory.getLogger(TestController.class);
+    @Resource
+    RedisTemplate redisTemplate;
+
+    @RequestMapping("/redis/set/{key}/{value}")
+    public String set(@PathVariable String key, @PathVariable String value) {
+        redisTemplate.opsForValue().set(key, value, 3600, TimeUnit.SECONDS);
+        LOG.info("key: {}, value: {}", key, value);
+        return "success";
+    }
+
+    @RequestMapping("/redis/get/{key}")
+    public Object get(@PathVariable String key) {
+        Object object = redisTemplate.opsForValue().get(key);
+        LOG.info("key: {}, value: {}", key, object);
+        return object;
+    }
 
     @RequestMapping("/hello")
     public String hello() {
