52e84678d1aa71879399d0c8f0cb189ec9e2e096
wychmod <545480453@qq.com>
2022-05-15 15:27:10 +0800

    doc/vote/**接口不需要做登录拦截

--- src/main/java/com/wychmod/wiki/config/SpringMvcConfig.java
+++ src/main/java/com/wychmod/wiki/config/SpringMvcConfig.java
@@ -24,6 +24,7 @@ public class SpringMvcConfig implements WebMvcConfigurer {
                         "/category/all",
                         "/ebook/list",
                         "/doc/all/**",
+                        "/doc/vote/**",
                         "/doc/find-content/**"
                 );
     }
